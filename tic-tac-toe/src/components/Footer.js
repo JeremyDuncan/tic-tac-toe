72f0bed46cedee60f925c509cc323676e84f8ca7
Jeremy Duncan <jduncan46@my.devry.edu>
2022-09-29 11:50:12 -0400

    added footer component

---
+++ tic-tac-toe/src/components/Footer.js
@@ -0,0 +1,15 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { Typography } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <Typography> Created by Jeremy Duncan </Typography>
+      <Typography> &copy;2022</Typography>
+    </div>
+  );
+};
+export default Footer;
