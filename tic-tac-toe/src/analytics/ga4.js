022cb601176c74b2ccc613ee3837e2a97f03a8b6
Jeremy Duncan <jduncan46@my.devry.edu>
2023-04-09 21:56:16 -0400

    removed export

--- tic-tac-toe/src/analytics/ga4.js
+++ tic-tac-toe/src/analytics/ga4.js
@@ -1,4 +1,4 @@
-export function send_ga4_event() {
+function send_ga4_event() {
   const email_time_stamp = Math.floor(Date.now() / 1000);
   const event_name = `CDN_EVENT_TEST_${email_time_stamp}`;
   gtag("event", `${event_name}`, {});
